c2df8054df539b9146f6d8a8253b7c4ef66745af
suet520 <suetchow@openknowledge.hk>
2024-08-21 23:51:10 +0800

    update login page

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+  public feedback = false;
+  public login = false;
 }
--- frontend/src/app/components/pages/feedback/feedback.component.ts
+++ frontend/src/app/components/pages/feedback/feedback.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-feedback',
+  selector: 'feedbackPage',
   templateUrl: './feedback.component.html',
   styleUrls: ['./feedback.component.css']
 })
