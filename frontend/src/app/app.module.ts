a82dbf4e55c1beede02794ad676062f9a6201351
suet520 <suetchow@openknowledge.hk>
2024-08-21 23:43:47 +0800

    update home page

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FeedbackComponent } from './components/pages/feedback/feedback.component';
+import { LoginComponent } from './components/pages/login/login.component';
+import { HeaderComponent } from './components/partials/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FeedbackComponent,
+    LoginComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
